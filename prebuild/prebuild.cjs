c422e8e5fb1165bc69448b7e3e394ed401f476f5
le.van.thanh <le.van.thanh@linecorp.com>
2024-01-09 16:01:30 +0700

    Update

--- prebuild/prebuild.cjs
+++ prebuild/prebuild.cjs
@@ -1,21 +1,22 @@
-const fs = require('fs');
+const fs = require('fs')
 
-const fileAPath = './prebuild/VPNavBarTitle.vue';
-const fileBPath = './node_modules/@vue/theme/src/vitepress/components/VPNavBarTitle.vue';
+function overwritingFile(fileAPath, fileBPath) {
+  try {
+    // Read the content of file A synchronously
+    const data = fs.readFileSync(fileAPath, 'utf-8')
 
-// Read the content of file A
-fs.readFile(fileAPath, 'utf-8', (err, data) => {
-  if (err) {
-    console.error(`Error reading file A: ${err}`);
-    return;
+    // Write the content of file A to file B, overwriting its content
+    fs.writeFileSync(fileBPath, data, 'utf-8')
+
+    console.log(`Content from file A successfully overwritten to file B.`)
+  } catch (error) {
+    console.error(`Error: ${error.message}`)
   }
+}
 
-  // Write the content of file A to file B, overwriting its content
-  fs.writeFile(fileBPath, data, 'utf-8', (writeErr) => {
-    if (writeErr) {
-      console.error(`Error writing to file B: ${writeErr}`);
-    } else {
-      console.log(`Content from file A successfully overwritten to file B.`);
-    }
-  });
-});
+function main() {
+  overwritingFile('./prebuild/VPNavBarTitle.vue', './node_modules/@vue/theme/src/vitepress/components/VPNavBarTitle.vue')
+ overwritingFile('./prebuild/variables.css', './node_modules/@vue/theme/src/core/styles/variables.css')
+}
+
+main()
