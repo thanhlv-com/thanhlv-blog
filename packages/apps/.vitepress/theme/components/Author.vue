76797c876c0da18026a3a59238a588f3853707fa
Thanhlv <lethanh9398@gmail.com>
2024-12-07 16:38:01 +0700

    add puml

--- packages/apps/.vitepress/theme/components/Author.vue
+++ packages/apps/.vitepress/theme/components/Author.vue
@@ -1,17 +1,21 @@
 <script setup lang='ts'>
-import { useData, useRoute } from 'vitepress'
-import { data as blogs } from './all.data'
-import { AUTHORS } from '@theme/components/constants'
-import { computed } from 'vue'
-import { VPTeamMembers } from 'vitepress/theme'
+import {useData, useRoute} from 'vitepress'
+import {data as blogs} from './all.data'
+import {AUTHORS} from '@theme/components/constants'
+import {computed} from 'vue'
+import {VPTeamMembers} from 'vitepress/theme'
 
-const { frontmatter } = useData()
-const { frontmatter: data } = useData()
+const {frontmatter} = useData()
+const {frontmatter: data} = useData()
 
 const route = useRoute()
 
 function findCurrentIndex() {
   return blogs.findIndex((p) => p.url === route.path)
+}
+
+function generateUniqueId() {
+  return `id-${Math.random().toString(36).substr(2, 9)}-${Date.now()}`;
 }
 
 const members = computed(() => blogs[findCurrentIndex()]?.authors?.map((key) => AUTHORS[key]) || [])
@@ -21,7 +25,7 @@ const members = computed(() => blogs[findCurrentIndex()]?.authors?.map((key) => 
 <template>
   <div v-if='frontmatter.authors'>
     <a class='sponsors-aside-text'>Tác giả</a>
-    <VPTeamMembers size='small' :members='members' />
+    <VPTeamMembers :key="generateUniqueId()" size='small' :members='members' />
   </div>
 </template>
 
