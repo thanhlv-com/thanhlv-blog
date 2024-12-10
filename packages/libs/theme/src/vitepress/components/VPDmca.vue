7b11e3c862b5f53e44a2dc42650065be5bdcc62a
Thanhlv <lethanh9398@gmail.com>
2024-12-10 10:00:10 +0700

    change lib

--- packages/libs/theme/src/vitepress/components/VPDmca.vue
+++ packages/libs/theme/src/vitepress/components/VPDmca.vue
@@ -1,6 +1,11 @@
+<script setup lang="ts">
+function href(): string {
+  return window.location.href
+}
+</script>
 <template>
   <p>
-    <a href="//www.dmca.com/Protection/Status.aspx?ID=8febc2ec-f016-49e9-8483-8b1afed383d4"
+    <a :href="'//www.dmca.com/Protection/Status.aspx?ID=8febc2ec-f016-49e9-8483-8b1afed383d4&cdnrdr=1&refurl=' + href()"
        title="DMCA.com Protection Status" class="dmca-badge">
       <img style="margin: auto" class="no-zoom"
            src="https://images.dmca.com/Badges/dmca_protected_sml_120m.png?ID=8febc2ec-f016-49e9-8483-8b1afed383d4"
--- packages/libs/theme/src/vitepress/components/VPFooter.vue
+++ packages/libs/theme/src/vitepress/components/VPFooter.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import {VTLink} from '../../core'
 import {useConfig} from '../composables/config'
+import VPDmca from "./VPDmca.vue";
 
 const {config} = useConfig()
 </script>
@@ -22,14 +23,7 @@ const {config} = useConfig()
     <p v-if="config.footer?.copyright" class="copyright">
       {{ config.footer.copyright }}
     </p>
-    <p>
-      <a href="//www.dmca.com/Protection/Status.aspx?ID=8febc2ec-f016-49e9-8483-8b1afed383d4"
-         title="DMCA.com Protection Status" class="dmca-badge">
-        <img style="margin: auto" class="no-zoom"
-        src="https://images.dmca.com/Badges/dmca_protected_sml_120m.png?ID=8febc2ec-f016-49e9-8483-8b1afed383d4"
-        alt="DMCA.com Protection Status" />
-      </a>
-    </p>
+    <VPDmca/>
 
   </div>
 </template>
