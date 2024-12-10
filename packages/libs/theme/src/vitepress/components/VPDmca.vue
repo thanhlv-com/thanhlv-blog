627e53aebd8e342f896630dc9c48c851135cf334
Thanhlv <lethanh9398@gmail.com>
2024-12-10 10:29:00 +0700

    change lib

--- packages/libs/theme/src/vitepress/components/VPDmca.vue
+++ packages/libs/theme/src/vitepress/components/VPDmca.vue
@@ -10,7 +10,7 @@ const dynamicHref = computed(() => {
 </script>
 <template>
   <p>
-    <a :href="dynamicHref"
+    <a target="_blank" :href="dynamicHref"
        title="DMCA.com Protection Status" class="dmca-badge">
       <img style="margin: auto" class="no-zoom"
            src="https://images.dmca.com/Badges/dmca_protected_sml_120m.png?ID=8febc2ec-f016-49e9-8483-8b1afed383d4"
