740e71bbfe9607b90d8d1fa41bc73b1d1682bb13
Thanhlv <lethanh9398@gmail.com>
2024-12-10 10:10:50 +0700

    change lib

--- packages/libs/theme/src/vitepress/components/VPDmca.vue
+++ packages/libs/theme/src/vitepress/components/VPDmca.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
-function href(): string {
-  return window.location.href
-}
+import {computed} from 'vue';
+import {useRoute} from 'vitepress';
+
+const route = useRoute();
+
+const dynamicHref = computed(() => {
+  return `//www.dmca.com/Protection/Status.aspx?ID=8febc2ec-f016-49e9-8483-8b1afed383d4&cdnrdr=1&refurl=${window.location.href}`;
+});
 </script>
 <template>
   <p>
-    <a :href="'//www.dmca.com/Protection/Status.aspx?ID=8febc2ec-f016-49e9-8483-8b1afed383d4&cdnrdr=1&refurl=' + href()"
+    <a :href="dynamicHref"
        title="DMCA.com Protection Status" class="dmca-badge">
       <img style="margin: auto" class="no-zoom"
            src="https://images.dmca.com/Badges/dmca_protected_sml_120m.png?ID=8febc2ec-f016-49e9-8483-8b1afed383d4"
