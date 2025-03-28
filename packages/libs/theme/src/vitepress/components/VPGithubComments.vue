76cf7144a17b7cdf306d96851a6996193fd32b0b
Solitarysp <solitarysp@gmail.com>
2025-03-28 22:44:41 +0700

    refactor VPGithubComments.vue to ensure script loading only occurs in a browser environment (#27)
    Co-authored-by: solit <lethanh9398@gmail.com>

--- packages/libs/theme/src/vitepress/components/VPGithubComments.vue
+++ packages/libs/theme/src/vitepress/components/VPGithubComments.vue
@@ -14,40 +14,45 @@ onMounted(() => {
 });
 
 const dynamicHref = computed(() => {
-  const scriptId = "custom-script-giscus-client"; // ID của script
-  const existingScript = document.getElementById(scriptId);
+  if (typeof document !== "undefined") {
+    const scriptId = "custom-script-giscus-client"; // ID của script
+    const existingScript = document.getElementById(scriptId);
 
-  if (existingScript) {
-    // Nếu script đã tồn tại, xóa và tải lại
-    existingScript.remove();
+    if (existingScript) {
+      // Nếu script đã tồn tại, xóa và tải lại
+      existingScript.remove();
+    }
   }
 
  setTimeout(() => {
-   // Tạo script mới
-   const script = document.createElement("script");
-   script.id = scriptId;
-   script.src = "https://giscus.app/client.js"; // URL của script
-   script.async = true;
-   script.setAttribute('data-repo', 'thanhlv-com/thanhlv-blog');
-   script.setAttribute('data-repo-id', 'R_kgDOLCoNuA');
-   script.setAttribute('data-category', 'General');
-   script.setAttribute('data-category-id', 'DIC_kwDOLCoNuM4Coi9r');
-   script.setAttribute('data-mapping', 'title');
-   script.setAttribute('data-strict', '0');
-   script.setAttribute('data-reactions-enabled', '1');
-   script.setAttribute('data-emit-metadata', '0');
-   script.setAttribute('data-input-position', 'top');
-   script.setAttribute('data-theme', 'light');
-   script.setAttribute('data-lang', 'vi');
-   script.setAttribute('data-loading', 'lazy');
-   script.setAttribute('crossorigin', 'anonymous');
+   if (typeof document !== "undefined") {
+     // Tạo script mới
+     const script = document.createElement("script");
+     script.id = scriptId;
+     script.src = "https://giscus.app/client.js"; // URL của script
+     script.async = true;
+     script.setAttribute('data-repo', 'thanhlv-com/thanhlv-blog');
+     script.setAttribute('data-repo-id', 'R_kgDOLCoNuA');
+     script.setAttribute('data-category', 'General');
+     script.setAttribute('data-category-id', 'DIC_kwDOLCoNuM4Coi9r');
+     script.setAttribute('data-mapping', 'title');
+     script.setAttribute('data-strict', '0');
+     script.setAttribute('data-reactions-enabled', '1');
+     script.setAttribute('data-emit-metadata', '0');
+     script.setAttribute('data-input-position', 'top');
+     script.setAttribute('data-theme', 'light');
+     script.setAttribute('data-lang', 'vi');
+     script.setAttribute('data-loading', 'lazy');
+     script.setAttribute('crossorigin', 'anonymous');
 
-   script.onload = () => {
-     console.log("Script loaded!");
-   };
+     script.onload = () => {
+       console.log("Script loaded!");
+     };
 
-   // Thêm script vào `head` hoặc `body`
-   document.head.appendChild(script);
+     // Thêm script vào `head` hoặc `body`
+     document.head.appendChild(script);
+   }
+
  },1000);
 
   return `${domain.value + route.path}`;
