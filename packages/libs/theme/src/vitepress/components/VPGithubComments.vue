f5768f20d7adbe36093d984b44aa0fcec31e0426
thanhlv <lethanh9398@gmail.com>
2025-03-30 23:34:51 +0700

    add event bus for theme change handling and update VPGithubComments.vue to use it

--- packages/libs/theme/src/vitepress/components/VPGithubComments.vue
+++ packages/libs/theme/src/vitepress/components/VPGithubComments.vue
@@ -1,19 +1,21 @@
 <script setup lang="ts">
-import {computed, onMounted, ref} from 'vue';
+import {computed, onMounted, onUnmounted, ref} from 'vue';
 import {useRoute} from 'vitepress';
+import eventBus from "../../core/components/eventBus";
 
 const route = useRoute();
 
 
 const domain = ref('');
 
-onMounted(() => {
-  // Lấy domain từ URL hiện tại
-  domain.value = window.location.hostname;
-  console.log('Domain Name:', domain.value);
-});
+const setComment = () => {
+  const storageKey = 'vitepress-theme-appearance'
+  let userPreference = localStorage.getItem(storageKey) || 'auto';
+  const query = window.matchMedia(`(prefers-color-scheme: dark)`)
+  const isDark = ref(
+    userPreference === 'auto' ? query.matches : userPreference === 'dark'
+  )
 
-const dynamicHref = computed(() => {
   const scriptId = "custom-script-giscus-client"; // ID của script
   const existingScript = document.getElementById(scriptId);
 
@@ -22,36 +24,52 @@ const dynamicHref = computed(() => {
     existingScript.remove();
   }
 
- setTimeout(() => {
-   // Tạo script mới
-   const script = document.createElement("script");
-   script.id = scriptId;
-   script.src = "https://giscus.app/client.js"; // URL của script
-   script.async = true;
-   script.setAttribute('data-repo', 'thanhlv-com/thanhlv-blog');
-   script.setAttribute('data-repo-id', 'R_kgDOLCoNuA');
-   script.setAttribute('data-category', 'General');
-   script.setAttribute('data-category-id', 'DIC_kwDOLCoNuM4Coi9r');
-   script.setAttribute('data-mapping', 'title');
-   script.setAttribute('data-strict', '1');
-   script.setAttribute('data-reactions-enabled', '1');
-   script.setAttribute('data-emit-metadata', '0');
-   script.setAttribute('data-input-position', 'top');
-   script.setAttribute('data-theme', 'light');
-   script.setAttribute('data-lang', 'vi');
-   script.setAttribute('data-loading', 'lazy');
-   script.setAttribute('crossorigin', 'anonymous');
+  setTimeout(() => {
+    // Tạo script mới
+    const script = document.createElement("script");
+    script.id = scriptId;
+    script.src = "https://giscus.app/client.js"; // URL của script
+    script.async = true;
+    script.setAttribute('data-repo', 'thanhlv-com/thanhlv-blog');
+    script.setAttribute('data-repo-id', 'R_kgDOLCoNuA');
+    script.setAttribute('data-category', 'General');
+    script.setAttribute('data-category-id', 'DIC_kwDOLCoNuM4Coi9r');
+    script.setAttribute('data-mapping', 'title');
+    script.setAttribute('data-strict', '1');
+    script.setAttribute('data-reactions-enabled', '1');
+    script.setAttribute('data-emit-metadata', '0');
+    script.setAttribute('data-input-position', 'top');
+    script.setAttribute('data-theme', isDark.value ? 'dark' : 'light');
+    script.setAttribute('data-lang', 'vi');
+    script.setAttribute('data-loading', 'lazy');
+    script.setAttribute('crossorigin', 'anonymous');
 
-   script.onload = () => {
-     console.log("Script loaded!");
-   };
+    script.onload = () => {
+      console.log("Script loaded!");
+    };
 
-   // Thêm script vào `head` hoặc `body`
-   document.head.appendChild(script);
- },1000);
+    // Thêm script vào `head` hoặc `body`
+    document.head.appendChild(script);
+  },1000);
+};
 
+const dynamicHref = computed(() => {
+  setComment();
   return `${domain.value + route.path}`;
 });
+
+
+onMounted(() => {
+  eventBus.addEventListener('event-change-theme', setComment);
+  // Lấy domain từ URL hiện tại
+  domain.value = window.location.hostname;
+  console.log('Domain Name:', domain.value);
+});
+
+onUnmounted(() => {
+  eventBus.removeEventListener('event-change-theme', setComment);
+});
+
 </script>
 <template>
   <div :data-href="dynamicHref" class="giscus"></div>
